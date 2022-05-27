28598377188a5b2152dc7e64cd3a30d45b157dff
Erzhena Mironova <33804626+erzhenami@users.noreply.github.com>
2022-05-27 22:40:32 +0300

    Третье (оптимальное) решение задачи
    Заведем словарь (Dictionary): ключ - символ, значение - количество раз, которое он встречается в строке.
    Сложность алгоритма - О(N)

---
+++ lesson-1-1.swift
@@ -0,0 +1,27 @@
+// Задача 1. Дана строка (в кодировке UTF-8). 
+// Найти самый часто встречающийся в ней символ. Если несколько символов встречаются одинаково часто, то можно вывести любой.
+
+import Foundation
+
+let symbols = Array(readLine()!)
+
+var ans = Character("")
+var anscnt = 0
+
+var dct: [Character : Int] = [:]
+
+for symbol in symbols {
+    if !dct.keys.contains(symbol) {
+        dct[symbol] = 0
+    } 
+    dct[symbol]! += 1
+}
+
+for (key, value) in dct {
+    if anscnt < value {
+        anscnt = value
+        ans = key
+    }
+}
+
+print(ans)
