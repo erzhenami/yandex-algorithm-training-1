bebb46223a0c8a81f38e382dbcb26f5a40e34df8
Erzhena Mironova <33804626+erzhenami@users.noreply.github.com>
2022-05-28 17:26:25 +0300

    Четвертое решение после отладки
    Поиск максимально часто встречающегося символа теперь происходит не в отдельном цикле, а внутри основного

--- lesson-1-1.swift
+++ lesson-1-1.swift
@@ -1,6 +1,5 @@
 // Задача 1. Дана строка (в кодировке UTF-8). 
 // Найти самый часто встречающийся в ней символ. Если несколько символов встречаются одинаково часто, то можно вывести любой.
-
 import Foundation
 
 let symbols = Array(readLine()!)
@@ -11,16 +10,15 @@ var anscnt = 0
 var dct: [Character : Int] = [:]
 
 for symbol in symbols {
+
     if !dct.keys.contains(symbol) {
         dct[symbol] = 0
     } 
     dct[symbol]! += 1
-}
-
-for (key, value) in dct {
-    if anscnt < value {
-        anscnt = value
-        ans = key
+    
+    if dct[symbol]! > anscnt {
+        ans = symbol
+        anscnt = dct[symbol]!
     }
 }
 
